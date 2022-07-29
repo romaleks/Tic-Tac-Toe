89799afe1f82923539ff1ae4e0efe8ed9905b265
IDDna <romanov1371@gmail.com>
2022-07-29 18:25:10 +0300

    Create game Object

--- script.js
+++ script.js
@@ -28,14 +28,36 @@ const Player = (symbol, gameBoard) => {
    }
 }
 
+const game = (() => {
+   WINNING_COMBINATIONS = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+   ]
+   
+   const playGame = () => {
+      let step = 0
+
+      boardCells.forEach(cell => {
+         cell.addEventListener('click', () => {
+            if (step % 2 === 0) firstPlayer.addSymbol(cell);
+            else secondPlayer.addSymbol(cell);
+            step++;
+         });
+      });
+   }
+
+   return {
+      playGame,
+   }
+})();
+
 const firstPlayer = Player('X', gameBoard);
 const secondPlayer = Player('O', gameBoard);
-let step = 0
 
-boardCells.forEach(cell => {
-   cell.addEventListener('click', () => {
-      if (step % 2 === 0) firstPlayer.addSymbol(cell);
-      else secondPlayer.addSymbol(cell);
-      step++;
-   })
-})
+game.playGame()
