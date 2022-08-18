bec2fb30f91c7f46d45ad59028c5a8c4ca4e8935
IDDna <romanov1371@gmail.com>
2022-08-18 18:23:29 +0300

    Update README file and convert js code with prettier

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# Tic-Tac-Toe
+# Todo App
+
+This project is a part of the "The Odin Project" [curriculum](https://www.theodinproject.com/).
+
+## Table of contents
+
+- [Overview](#overview)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+
+## Overview
+
+### Screenshot
+
+![Solution Preview](./dist/img/preview.png)
+
+### Links
+
+- Repository URL: [View Code](https://github.com/romaleks/Tic-Tac-Toe)
+- Live Site URL: [Live Preview](https://romaleks.github.io/Tic-Tac-Toe/)
+
+## My process
+
+### Built with
+
+- CSS Custom Properties
+- JavaScript Factory Functions
+- JavaScript Revealing Module Pattern
+- Visual Studio Code
--- script.js
+++ script.js
@@ -1,129 +1,133 @@
-const boardCells = document.querySelectorAll('.grid__item');
-const form = document.querySelector('form');
+const boardCells = document.querySelectorAll(".grid__item");
+const form = document.querySelector("form");
 
 const gameBoard = (() => {
-   const board = ['', '', '', '', '', '', '', '', ''];
+  const board = ["", "", "", "", "", "", "", "", ""];
 
-   const displayContent = () => {
-      let cellIndex = 0;
-      boardCells.forEach(cell => cell.textContent = gameBoard.board[cellIndex++]);
-   }
+  const displayContent = () => {
+    let cellIndex = 0;
+    boardCells.forEach(
+      (cell) => (cell.textContent = gameBoard.board[cellIndex++])
+    );
+  };
 
-   return {
-      board,
-      displayContent
-   }
+  return {
+    board,
+    displayContent,
+  };
 })();
 
 const Player = (symbol, gameBoard) => {
-   const addSymbol = (cellIndex) => {
-      gameBoard.board[cellIndex] = symbol;
-      gameBoard.displayContent();
-   }
+  const addSymbol = (cellIndex) => {
+    gameBoard.board[cellIndex] = symbol;
+    gameBoard.displayContent();
+  };
 
-   return {
-      symbol,
-      score: 0,
-      addSymbol
-   }
-}
+  return {
+    symbol,
+    score: 0,
+    addSymbol,
+  };
+};
 
 const game = (() => {
-   const WINNING_COMBINATIONS = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6]
-   ]
-   const boardGrid = document.querySelector('.board__grid');
-   const restartBtn = document.querySelector('.board__restart-btn');
-   const newRoundBtn = document.querySelector('.round-btn');
-   let step = 0;
+  const WINNING_COMBINATIONS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  const boardGrid = document.querySelector(".board__grid");
+  const restartBtn = document.querySelector(".board__restart-btn");
+  const newRoundBtn = document.querySelector(".round-btn");
+  let step = 0;
 
-   const restartGame = () => {
-      gameBoard.board = ['', '', '', '', '', '', '', '', ''];
-      gameBoard.displayContent();
-      boardGrid.classList.remove('active');
-      boardCells.forEach(cell => cell.style.pointerEvents = 'all');
-      step = 0;
-      firstPlayer.score = 0;
-      secondPlayer.score = 0;
-      document.querySelectorAll('.player__score').forEach(score => {
-         score.textContent = '0';
-      })
-   }
+  const restartGame = () => {
+    gameBoard.board = ["", "", "", "", "", "", "", "", ""];
+    gameBoard.displayContent();
+    boardGrid.classList.remove("active");
+    boardCells.forEach((cell) => (cell.style.pointerEvents = "all"));
+    step = 0;
+    firstPlayer.score = 0;
+    secondPlayer.score = 0;
+    document.querySelectorAll(".player__score").forEach((score) => {
+      score.textContent = "0";
+    });
+  };
 
-   const beginNewGame = () => {
-      gameBoard.board = ['', '', '', '', '', '', '', '', ''];
-      gameBoard.displayContent();
-      boardGrid.classList.remove('active');
-      boardCells.forEach(cell => cell.style.pointerEvents = 'all');
-   }
+  const beginNewGame = () => {
+    gameBoard.board = ["", "", "", "", "", "", "", "", ""];
+    gameBoard.displayContent();
+    boardGrid.classList.remove("active");
+    boardCells.forEach((cell) => (cell.style.pointerEvents = "all"));
+  };
 
-   const checkWinning = (symbol) => {
-      return WINNING_COMBINATIONS.some(combination => {
-         return combination.every(index => {
-            return boardCells[index].textContent === symbol;
-         });
+  const checkWinning = (symbol) => {
+    return WINNING_COMBINATIONS.some((combination) => {
+      return combination.every((index) => {
+        return boardCells[index].textContent === symbol;
       });
-   }
+    });
+  };
 
-   const endGame = (winner) => {
-      const winnerScore = document.querySelector('#' + winner.symbol).textContent = ++winner.score;
-      boardCells.forEach(cell => cell.style.pointerEvents = 'none');
-      setTimeout(() => {
-         boardGrid.classList.add('active')
-      }, 1500)
-   }
+  const endGame = (winner) => {
+    const winnerScore = (document.querySelector(
+      "#" + winner.symbol
+    ).textContent = ++winner.score);
+    boardCells.forEach((cell) => (cell.style.pointerEvents = "none"));
+    setTimeout(() => {
+      boardGrid.classList.add("active");
+    }, 500);
+  };
 
-   const playGame = () => {
-      boardCells.forEach(cell => {
-         cell.addEventListener('click', () => {
-            const cellIndex = cell.getAttribute('data-index');
-            if (gameBoard.board[cellIndex] === '') {
-               if (step % 2 === 0) {
-                  firstPlayer.addSymbol(cellIndex);
-                  if (checkWinning(firstPlayer.symbol)) endGame(firstPlayer);
-               } else {
-                  secondPlayer.addSymbol(cellIndex);
-                  if (checkWinning(secondPlayer.symbol)) endGame(secondPlayer);
-               }
-               step++;
-            }
-         });
+  const playGame = () => {
+    boardCells.forEach((cell) => {
+      cell.addEventListener("click", () => {
+        const cellIndex = cell.getAttribute("data-index");
+        if (gameBoard.board[cellIndex] === "") {
+          if (step % 2 === 0) {
+            firstPlayer.addSymbol(cellIndex);
+            if (checkWinning(firstPlayer.symbol)) endGame(firstPlayer);
+          } else {
+            secondPlayer.addSymbol(cellIndex);
+            if (checkWinning(secondPlayer.symbol)) endGame(secondPlayer);
+          }
+          step++;
+        }
       });
-   }
+    });
+  };
 
-   return {
-      restartBtn,
-      newRoundBtn,
-      playGame,
-      restartGame,
-      beginNewGame
-   }
+  return {
+    restartBtn,
+    newRoundBtn,
+    playGame,
+    restartGame,
+    beginNewGame,
+  };
 })();
 
-const firstPlayer = Player('X', gameBoard);
-const secondPlayer = Player('O', gameBoard);
+const firstPlayer = Player("X", gameBoard);
+const secondPlayer = Player("O", gameBoard);
 
-form.addEventListener('submit', (e) => {
-   const popup = document.querySelector('.popup');
-   const main = document.querySelector('main');
-   const firstPlayerInput = document.querySelector('#first_name');
-   const secondPlayerInput = document.querySelector('#second_name');
-   const firstPlayerName = document.querySelector('#first_player');
-   const secondPlayerName = document.querySelector('#second_player');
+form.addEventListener("submit", (e) => {
+  const popup = document.querySelector(".popup");
+  const main = document.querySelector("main");
+  const firstPlayerInput = document.querySelector("#first_name");
+  const secondPlayerInput = document.querySelector("#second_name");
+  const firstPlayerName = document.querySelector("#first_player");
+  const secondPlayerName = document.querySelector("#second_player");
 
-   popup.classList.add('inactive');
-   main.classList.add('active');
-   firstPlayerName.textContent = firstPlayerInput.value;
-   secondPlayerName.textContent = secondPlayerInput.value;
-   e.preventDefault();
-   game.playGame();
-   game.restartBtn.onclick = game.restartGame;
-   game.newRoundBtn.onclick = game.beginNewGame;
-})
+  popup.classList.add("inactive");
+  main.classList.add("active");
+  firstPlayerName.textContent = firstPlayerInput.value;
+  secondPlayerName.textContent = secondPlayerInput.value;
+  e.preventDefault();
+  game.playGame();
+  game.restartBtn.onclick = game.restartGame;
+  game.newRoundBtn.onclick = game.beginNewGame;
+});
